d2c1a1ce22e1242500dee982e8299622698a7b3a
firefrost91 <49150875+firefrost91@users.noreply.github.com>
2020-02-08 22:46:35 +0530

    Create main.c

---
+++ Lab5/Ex-1 Reading_time/main.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+//#include"Creditc.h"
+//#include"readingtime.c"
+
+// struct Creditc{
+//  	char bank_code[6];
+// 	char expiry_date[8];
+// 	char first_name[20];
+// 	char last_name[20];
+// };   double card_no;
+
+int main()
+{
+   timeForReadingTest("10.txt" , "new.txt");
+}
