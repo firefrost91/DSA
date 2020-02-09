ce345df2f98e57e2b128cd23e59f35160deeba3c
firefrost91 <49150875+firefrost91@users.noreply.github.com>
2020-02-09 17:28:16 +1100

    Create main.c

---
+++ Lab5/Ex-2/main.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+//#include"time_space_measurement.c"
+//#include"Creditc.h"
+//#include"readingtime.c"
+
+// struct Creditc{
+//  	char bank_code[6];
+// 	char expiry_date[8];
+// 	char first_name[20];
+// 	char last_name[20];
+// };   double card_no;
+
+int main()
+{
+   timeForSortingTest("10.txt" , "timeforsorting.txt");
+}
