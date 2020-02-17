7b0a1d1fcca9f97334c0f773337f9fc8257a2c03
firefrost91 <49150875+firefrost91@users.noreply.github.com>
2020-02-17 16:46:58 +0530

    Create merge.h

---
+++ Lab6/merge.h
@@ -0,0 +1,10 @@
+#include <element.h>
+void merge(Element Ls1[], int sz1, Element Ls2[], int sz2, Element Ls[]);
+void mergeSortRec(Element arr[],int l, int r);
+void mergeSort(Element arr[],int n);
+void mergeSortIter(Element arr[],int n);
+int min(int a,int b);
+
+
+int iterspace;
+int recspace;
