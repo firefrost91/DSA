caa06f58b810b82e621eeeefdef00efb0b2f96f5
Anshuman Mishra <f20170622@pilani.bits-pilani.ac.in>
2020-02-26 20:38:30 +0530

    Create 31. Modulus_hashing.c

---
+++ 31. Modulus_hashing.c
@@ -0,0 +1,121 @@
+#include<stdio.h>
+#include<stdlib.h>
+static int i = 0 ;
+struct node{
+    int data ; 
+    struct node* next ; 
+}; 
+struct node* add_to_empty (int x){
+   struct node* temp ;
+   temp=(struct node*)malloc(sizeof(struct node)); 
+   temp->data=x ;
+   temp->next=NULL ;
+   return temp ; 
+}
+struct node* add_at_first (struct node* first , int x){
+    struct node * temp ; 
+    temp=(struct node* )malloc(sizeof(struct node)); 
+    if(first==NULL){
+        temp=add_to_empty(x);
+        return temp ; 
+    }
+    temp->data=x ; 
+    temp->next=first ; 
+    first = temp ; 
+    return first ; 
+} 
+struct node* add_at_last ( struct node* first , int x){
+    struct node* temp , *p ; 
+    temp=(struct node*)malloc(sizeof(struct node)); 
+    if(first==NULL){
+        temp=add_to_empty(x);
+        return temp ; 
+    }
+    temp->data= x ;
+    p=first ; 
+    while(p->next!=NULL){
+        p=p->next ; 
+    }
+    p->next=temp ; 
+    temp->next=NULL ; 
+    return first ; 
+}
+struct node* sorted_insert(struct node* first , int x  ){
+ struct node* temp, *p , *q  ;
+ p=first ; 
+ int i=0  ; 
+ temp=(struct node*)malloc(sizeof(struct node));
+ if(first==NULL){
+        temp=add_to_empty(x);
+        return temp ; 
+    }
+ temp->data=x ; 
+ if(x<p->data){
+     first=add_at_first(first , x); 
+     return first ; 
+ }
+ while(p!=NULL){
+    i++;
+     q=p->next ; 
+     if(q!=NULL && x<q->data){
+         temp->next = q; 
+         p->next=temp ; 
+         return first ; 
+     }
+     p=p->next ; 
+ }
+ first = add_at_last(first ,  x);
+ return first ; 
+}
+void display(struct node* first){
+    struct node* p = first ; 
+    while(p!=NULL){
+        printf("%d " , p->data); 
+        p=p->next ; 
+    }
+ printf("\n"); 
+    
+}
+void Iterative_search(struct node* first , int key){
+    int i = 0 ;
+    struct node* p = first ; 
+    while(p!=NULL){
+        if(p->data==key){
+            printf("Element is at index %d \n" , i ); 
+            return ;     }
+        i++;
+        p=p->next;
+    }
+   printf("Element not found "); 
+   return ; 
+}
+int hash(int key){
+    return key%10 ; 
+}
+void insert_in_hash(struct node* HT[] , int key ){
+    int index=hash(key );
+    struct node* first  ;
+    HT[index] = sorted_insert(HT[index] , key) ;
+     
+}
+int main(){
+  struct node *HT[20]; 
+  int n = sizeof(HT)/sizeof(struct node *); 
+//   printf("%d" , n ); 
+   for(i=0 ; i < n ; i ++){
+      HT[i]=NULL ; 
+  }
+  struct node* first;
+  
+ insert_in_hash(HT , 12); 
+ insert_in_hash(HT , 13); 
+ insert_in_hash(HT , 22); 
+  for(i=0 ; i < n ; i ++){
+      if(HT[i]==NULL){
+          printf("Empty");
+      }
+      display(HT[i]) ;
+  }
+  
+ 
+}
